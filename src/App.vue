9785d9267aa019f536d2e8edc91b5617bb6a1a63
wuhui <1203905045@qq.com>
2018-10-18 15:07:38 +0800

    完成了底部和顶部

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <mt-header fixed title="固定在顶部"></mt-header>
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">搜索</span>
+			</a>
+		</nav>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+
+import './assets/mui/css/mui.min.css'
+import './assets/mui/css/icons-extra(1).css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
